6ebfe5770de03624ae81f6cf2d1a54e7ebe71ba7
PhilConnected <88433497+PhiStar1@users.noreply.github.com>
2024-11-18 20:12:46 +0100

    Add files via upload

---
+++ WebConnect/WebConnectRecoveryTool.vb
@@ -0,0 +1,16 @@
+﻿Public Class WebConnectRecoveryTool
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            ResetSettings.ShowDialog()
+        Catch ex As Exception
+            ResetSettingsnotavailable.ShowDialog()
+
+        End Try
+
+
+    End Sub
+
+    Private Sub WebConnectRecoveryTool_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
