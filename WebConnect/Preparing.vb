9840a9333463eaad5cfb7843a05a10458bfcf030
PhilConnected <88433497+PhiStar1@users.noreply.github.com>
2024-11-18 19:08:41 +0100

    Delete WebConnect directory for changing the system files

--- WebConnect/Preparing.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Preparing
-    Private Sub Preparing_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Text = "Vorbereiten"
-        Label1.Text = "App wird gestartet"
-    End Sub
-End Class
