0616cbdb4033747124ab8c41206f711ddc53a79d
PhilConnected <88433497+PhiStar1@users.noreply.github.com>
2025-05-30 23:37:05 +0200

    Delete WebConnect directory

--- WebConnect/ResettingSettingsnotavailable.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class ResettingSettingsnotavailable
-    Private Sub ResettingSettingsnotavailable_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
--- WebConnect/WebViewNotInitializedError.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class WebViewNotInitializedError
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-        Me.Close()
-    End Sub
-
-    Private Sub WebViewNotInitializedError_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
